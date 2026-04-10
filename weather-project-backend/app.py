26237b3eff0115a8cdebe737c0fd05df920162bf
DBG13452 <163539586+DBG13452@users.noreply.github.com>
2026-04-10 21:25:45 +0700

    Add files via upload

--- weather-project-backend/app.py
+++ weather-project-backend/app.py
@@ -1015,6 +1015,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://hilarious-lollipop-81bb12.netlify.app",
+        "https://dbg13452.github.io",
     ],
     allow_credentials=True,
     allow_methods=["*"],
