9e6e2d13a414262f8a781004d483062c8640d007
DBG13452 <163539586+DBG13452@users.noreply.github.com>
2026-04-10 13:36:58 +0700

    Add files via upload

--- weather-project-backend/app.py
+++ weather-project-backend/app.py
@@ -927,6 +927,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://enchanting-blancmange-ddbc5b.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
